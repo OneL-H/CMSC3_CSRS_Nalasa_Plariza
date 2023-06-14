3da5c58535f5dfbcc977cee470f95e6d1f98257f
OneL-H <117259320+OneL-H@users.noreply.github.com>
2023-06-15 01:37:47 +0800

    frontend
    looks pretty okay just needs logic and info format idk

---
+++ student_info.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 14, 2023 at 09:37 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `csrs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_info`
+--
+
+CREATE TABLE `student_info` (
+  `stud_num` varchar(10) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `units_enlisted` int(3) NOT NULL,
+  `didILoginForTheFirstTime` tinyint(1) NOT NULL DEFAULT 1,
+  `yearlevel` int(1) NOT NULL,
+  `fname` varchar(64) NOT NULL,
+  `mname` varchar(64) NOT NULL,
+  `lname` varchar(64) NOT NULL,
+  `address1` varchar(100) NOT NULL,
+  `address2` varchar(100) NOT NULL,
+  `bdate` date NOT NULL,
+  `college` varchar(4) NOT NULL DEFAULT '0',
+  `degprog` varchar(64) NOT NULL DEFAULT '0',
+  `sex` varchar(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_info`
+--
+
+INSERT INTO `student_info` (`stud_num`, `password`, `units_enlisted`, `didILoginForTheFirstTime`, `yearlevel`, `fname`, `mname`, `lname`, `address1`, `address2`, `bdate`, `college`, `degprog`, `sex`) VALUES
+('2022-03484', 'gian', 0, 0, 1, 'Violette Gwen Rai', 'Esperancilla', 'Rosales', 'Tipaz Road, Magugpo East, Tagum City', '', '2003-08-15', 'CSM', 'BS Biology', 'F'),
+('2022-05204', 'ilovegwen', 0, 0, 1, 'Gian Paolo', 'Debarbo', 'Plariza', 'B1 L1 P3 Niagara Falls St., Wellspring Village, Catalunan Pequeno, Davao City', '', '2004-02-01', 'CSM', 'BS Computer Science', 'M'),
+('2022-12345', '12345', 0, 0, 1, 'Juan', 'Jose', 'Dela Cruz', 'Manila, Davao City', '', '2010-10-10', 'CHSS', 'BA Communication and Media Arts', 'M');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student_info`
+--
+ALTER TABLE `student_info`
+  ADD PRIMARY KEY (`stud_num`) USING BTREE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
